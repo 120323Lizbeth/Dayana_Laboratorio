925477d4ef96387ffce628a85d44f9c1d42bd5c1
120323Lizbeth <20240394@ricaldone.edu.sv>
2025-08-22 10:45:17 -0600

    subir laboratorio

---
+++ src/main/java/Dayana_20240394/Dayana_20240394/Dayana20240394Application.java
@@ -0,0 +1,13 @@
+package Dayana_20240394.Dayana_20240394;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dayana20240394Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dayana20240394Application.class, args);
+	}
+
+}
---
+++ src/test/java/Dayana_20240394/Dayana_20240394/Dayana20240394ApplicationTests.java
@@ -0,0 +1,13 @@
+package Dayana_20240394.Dayana_20240394;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dayana20240394ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
